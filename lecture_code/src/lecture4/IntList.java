00484d6a5655ec9a8b8a6330c6cb916fc9b653d4
ChisatoLycoris GitHub <changarm429@gmail.com>
2023-02-12 20:35:36 +0800

    lecture code lecture 4~8

---
+++ lecture_code/src/lecture4/IntList.java
@@ -0,0 +1,136 @@
+package lecture4;
+public class IntList {
+    public int first;
+    public IntList rest;
+
+    public IntList(int f, IntList r){
+        first = f;
+        rest = r;
+    }
+    
+    /* Return the size of the list using... Recursion! */
+    public int size() {
+    	if (rest == null) {
+    		return 1;
+    	}
+    	return 1 + this.rest.size();
+    }
+    
+    /* Return the size of the list using no Recursion!*/
+    public int iterationSize() {
+    	IntList p = this;
+    	int totalSize = 0;
+    	while(p != null) {
+    		totalSize += 1;
+    		p = p.rest;
+    	}
+    	return totalSize;
+    }
+    
+    /* Return the ith in the IntList using Recursion*/
+    public int get(int i) {
+    	if (i == 0) {
+    		return this.first;
+    	}
+    	return this.rest.get(i-1);
+    }
+    
+    /* Return the ith in the IntList using no Recursion*/
+    public int iterationGet(int i) {
+    	IntList p = this;
+    	for (int j = 0; j < i; j++) {
+    		p = p.rest;
+    	}
+    	return p.first;
+    }
+
+    /*Extra IntList Practice*/
+	/** Returns an IntList identical to L, but with
+	 * each element incremented by x. L is not allowed
+	 * to change. */
+    public static IntList incrList(IntList L, int x) {
+    	int size = L.iterationSize();
+    	IntList Q = new IntList(L.iterationGet(size - 1) + x, null);
+    	for (int i = size - 2; i > -1; i-- ) {
+    		Q = new IntList(L.iterationGet(i) + x,Q);
+    	}
+    	return Q;
+    }
+    
+    /*Extra IntList Practice*/
+	/** Returns an IntList identical to L, but with
+	 * each element incremented by x. Not allowed to use
+	 * the 'new' keyword. */
+    public static IntList dincrList(IntList L, int x) {
+    	IntList Q = L;
+    	for (int i = 0; i < L.iterationSize(); i++) {
+    		Q.first = L.iterationGet(i) + x;
+    		Q = Q.rest;
+    	}
+    	Q = L;
+    	return Q;
+    }
+    
+    public static IntList reverse(IntList L) {
+    	IntList Q = new IntList(L.first, null);
+    	L = L.rest;
+    	while (L != null) {
+    		IntList temp = Q;
+    		Q = L;
+    		L = L.rest;
+    		Q.rest = temp;
+    	}
+    	return Q;
+    }
+    
+    public static void main(String[] args) {
+        // IntList L = new IntList();
+        // L.first = 5;
+        // L.rest = null;
+
+        // L.rest = new IntList();
+        // L.rest.first = 10;
+
+        // L.rest.rest = new IntList();
+        // L.rest.rest.first = 15;
+
+        IntList L = new IntList(15, null);
+        L = new IntList(10, L);
+        L = new IntList(5, L);
+        
+//        System.out.println(L.size());
+//        System.out.println(L.iterationSize());
+//        System.out.println(L.get(2));
+//        System.out.println(L.get(1));
+//        System.out.println(L.get(0));
+//        System.out.println(L.iterationGet(2));
+//        System.out.println(L.iterationGet(1));
+//        System.out.println(L.iterationGet(0));
+//        System.out.println("==========================");
+//        IntList Q = incrList(L, 2);
+//        System.out.println("Q:" + Q);
+//        System.out.println(Q.iterationGet(2));
+//        System.out.println(Q.iterationGet(1));
+//        System.out.println(Q.iterationGet(0));
+//        
+//        System.out.println("L:" + L);
+//        System.out.println(L == Q);
+//        System.out.println(L.iterationGet(2));
+//        System.out.println(L.iterationGet(1));
+//        System.out.println(L.iterationGet(0));
+//        
+//        System.out.println("==========================");
+//        IntList R = dincrList(L, 2);
+//        System.out.println("R:" + R);
+//        System.out.println(R.iterationGet(2));
+//        System.out.println(R.iterationGet(1));
+//        System.out.println(R.iterationGet(0));
+//        
+//        System.out.println("L:" + L);
+//        System.out.println(L == R);
+//        System.out.println(L.iterationGet(2));
+//        System.out.println(L.iterationGet(1));
+//        System.out.println(L.iterationGet(0));
+        
+    }
+}
---
+++ lecture_code/src/lecture6/GenericSLList.java
@@ -0,0 +1,128 @@
+package lecture6;
+/*An GenericSLList is a list of Generic Type, or an Object*/
+public class GenericSLList<GenericType> {
+	
+	/**
+	 * Nested classes are useful when a class doesn't stand on its own
+	 * and is obvious subordinate to another class.
+	 * Make the nested class private if other classes should never use the nested class.
+	 * */
+	private class StuffNode {
+		public GenericType item;
+	    public StuffNode next;
+
+	    public StuffNode(GenericType i, StuffNode n){
+	        item = i;
+	        next = n;
+	    }
+	}
+	
+	/* The first item (if it exists) is at sentinel.next*/
+	private StuffNode sentinel;
+	private int size;
+	
+	public GenericSLList() {
+		sentinel = new StuffNode(null, null);
+		size = 0;
+	}
+	
+	public GenericSLList(GenericType x) {
+		sentinel = new StuffNode(null, null);
+		sentinel.next = new StuffNode(x, null);
+		size = 1;
+	}
+	
+	/* Adds x to the front of the list*/
+	public void addFirst(GenericType x) {
+		sentinel.next = new StuffNode(x, sentinel.next);
+		size += 1;
+	}
+	
+	public GenericType getFirst() {
+		return sentinel.next.item;
+	}
+	
+	/** Adds an item to the end of the list */
+	public void addLast(GenericType x) {
+		size += 1;
+		
+		StuffNode temp = sentinel;
+		
+		/* move temp until it reaches the end of the list */
+		while (temp.next != null) {
+			temp = temp.next;
+		}
+		temp.next = new StuffNode(x, null);
+	}
+	
+//	/** Helper method using by the recursiveSize with no arguments*/
+//	private int recursiveSize(StuffNode p) {
+//		if(p.next == null) {
+//			return 1;
+//		} else {
+//			return 1 + recursiveSize(p.next);
+//		}
+//	}
+//	
+//	/** Returns the size of the SLList using Recursive and helper method*/
+//	public int recursiveSize() {
+//		return recursiveSize(sentinel.next);
+//	}
+//	
+//	/** Returns the size of the SLList using no Recursive*/
+//	public int size() {
+//		IntNode temp = sentinel.next;
+//		int count = 1;
+//		while (temp.next != null) {
+//			temp = temp.next;
+//			count += 1;
+//		}
+//		return count;
+//	}
+	
+	/* Maintain a special size variable that caches the size of the list
+	 * TANSTAAFl, but spreading the work over each add call is a net win in almost any circumstance*/
+	public int size() {
+		return size;
+	}
+	
+	public void print() {
+		System.out.println("The boss doesn't know what he's doing");
+		for (StuffNode p = sentinel.next; p != null; p = p.next) {
+			System.out.print(p.item + " ");
+		}
+		System.out.println();
+	}
+
+	public GenericType removeFirst() {
+		if (sentinel.next == null) {
+			return null;
+		}
+		GenericType first = sentinel.next.item;
+		sentinel.next = sentinel.next.next;
+		return first;
+	}
+
+	public GenericType removeLast() {
+		if (sentinel.next == null) {
+			return null;
+		}
+		StuffNode target = sentinel;
+		while (target.next.next != null) {
+			target = target.next;
+		}
+		GenericType last = target.next.item;
+		target.next = null;
+		return last;
+	}
+	
+	public static void main(String[] args) {
+		/* Create a list of String */
+		GenericSLList<String> L = new GenericSLList<>("World");
+		L.addFirst("Hello");
+		L.addLast("!");
+		System.out.println(L.size());
+		
+	}
+
+}
