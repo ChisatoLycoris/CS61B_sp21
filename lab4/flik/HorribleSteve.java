eaed542700806ce7f5c54b3389ec7710623a9e42
ChisatoLycoris GitHub <changarm429@gmail.com>
2023-02-18 00:15:12 +0800

    Lab 4: Debugging

--- lab4/flik/HorribleSteve.java
+++ lab4/flik/HorribleSteve.java
@@ -10,5 +10,9 @@ public class HorribleSteve {
             }
         }
         System.out.println("i is " + i);
+
+        // System.out.println(Integer.valueOf(127) == Integer.valueOf(127));
+        // System.out.println(Integer.valueOf(128) == Integer.valueOf(128));
+
     }
 }
