3bd1861ad6dc3bc90f613af6c4e104412c2f0a9c
ChisatoLycoris GitHub <changarm429@gmail.com>
2023-02-18 04:16:10 +0800

    lecture12 code

---
+++ lecture_code/src/lecture12/ArgsSum.java
@@ -0,0 +1,17 @@
+package lecture12;
+
+public class ArgsSum {
+    /**
+     * Prints out the sum of arguments, assuming they are integers.
+     * 測試
+     */
+    public static void main(String[] args) {
+        int index = 0;
+        int sum = 0;
+        while (index < args.length) {
+            sum = sum + Integer.parseInt(args[index]);
+            index += 1;
+        }
+        System.out.println(sum);
+    }
+}
